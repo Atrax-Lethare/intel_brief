21875f374cfd7f43360fa43fc2c6de7d4f4102bb
Abhishek Biradar <programmeratrax@gmail.com>
2025-12-02 21:41:26 +0530

    Update scripts.js

--- UPSC Relevant Section/scripts.js
+++ UPSC Relevant Section/scripts.js
@@ -288,7 +288,7 @@
                             <button class="btn-deep-dive" onclick="openDeepDive('${safeTitle}', articlesDB['${dataKey}'].articles[${index}].deepDive)">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon></svg> Read Deep Dive
                             </button>
-                            <a href="${article.wikiLink}" target="_blank" class="btn-wiki" title="Refer to this Wikipedia page to read about all the related concepts.">
+                            <a href="${article.wikiLink}" target="_blank" class="btn-wiki" title="Refer to this Wikipedia page to read about several related concepts.">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg> Wiki
                             </a>
                             ${article.originalLink ? `<a href="${article.originalLink}" target="_blank" class="btn-outline">Original Source &nearr;</a>` : ''}
@@ -336,3 +336,4 @@
             document.addEventListener('keydown', (e) => { if (e.key === 'Escape') closeModalDirect(); });
 
         });
+
